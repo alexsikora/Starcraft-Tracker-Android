4f43c8da77bbfbeb3133ae647051c51c40b1effb
Max Docauer <docauer2@gmail.com>
2012-02-12 17:49:08 -0600

    First Commit

---
+++ src/illinois/sweng/sctracker/SC2TrackerActivity.java
@@ -0,0 +1,13 @@
+package illinois.sweng.sctracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SC2TrackerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
