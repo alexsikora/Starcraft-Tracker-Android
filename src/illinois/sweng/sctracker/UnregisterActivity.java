01a930d54fc73e2362496bb37491bd763fdfc03a
Max Docauer <docauer2@gmail.com>
2012-02-16 19:15:15 -0600

    added activities

---
+++ src/illinois/sweng/sctracker/UnregisterActivity.java
@@ -0,0 +1,12 @@
+package illinois.sweng.sctracker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class UnregisterActivity extends Activity {
+
+}
