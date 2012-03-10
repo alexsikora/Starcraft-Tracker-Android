d43e84baa868fba8d5f2e399a7d7023555607bb8
Joel Ferm <jtferm@gmail.com>
2012-03-10 13:37:04 -0600

    removed unused imports

--- src/illinois/sweng/sctracker/RegisterActivity.java
+++ src/illinois/sweng/sctracker/RegisterActivity.java
@@ -3,14 +3,11 @@ package illinois.sweng.sctracker;
 import java.util.List;
 
 import org.apache.http.NameValuePair;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
--- src/illinois/sweng/sctracker/ServerCommunicator.java
+++ src/illinois/sweng/sctracker/ServerCommunicator.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
-
 import java.util.ArrayList;
 import java.util.List;
 
