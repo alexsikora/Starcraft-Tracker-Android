fa80dfa6931c543f28a17aceb2729485a56ec8f7
Joel Ferm <jtferm@gmail.com>
2012-03-11 22:59:25 -0500

    Merged

--- src/illinois/sweng/sctracker/PlayerStatusActivity.java
+++ src/illinois/sweng/sctracker/PlayerStatusActivity.java
@@ -1,33 +1,71 @@
 package illinois.sweng.sctracker;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import org.json.JSONArray;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class PlayerStatusActivity extends DelegateActivity {
-
+public class PlayerStatusActivity extends Activity {
+	static String TAG = "playerStatusActivity";
+	String data;
+	JSONObject player;
+	JSONObject playerData;
+	
+	String handle = "";
+	Object picture = "";
+	String name = "";
+	String race = "";
+	String team = "";
+	String nationality = "";
+	String elo;
+	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.playerstatus);
-
+        setContentView(R.layout.playerstatus);
+        
+        Bundle extras = getIntent().getExtras();
+        if(extras == null)
+        	return; // might want to log here - indicate extras was never assigned any data
+        
+        else {
+	        data = extras.getString("player");
+	        try{
+	        	player = new JSONObject(data);
+	        	playerData = player.getJSONObject("fields");
+	        	
+	        	handle = playerData.getJSONObject("handle").toString();
+	        	//picture = playerData.getJSONObject("picture").toString();
+	        	name = playerData.getJSONObject("name").toString();
+	        	race = playerData.getJSONObject("race").toString();
+	        	team = playerData.getJSONObject("team").toString();
+	        	nationality = playerData.getJSONObject("nationality").toString();
+	        	elo = playerData.getJSONObject("elo").toString();
+	        	
+	        } catch (JSONException e){
+	        	e.printStackTrace();
+	        }
+	        
+	        TextView t = (TextView)findViewById(R.id.textView1);
+	        t.append(handle);
+	        t = (TextView)findViewById(R.id.textView2);
+	        t.append(name);
+	        t = (TextView)findViewById(R.id.textView3);
+	        t.append(race);
+	        t = (TextView)findViewById(R.id.textView4);
+	        t.append(team);
+	        t = (TextView)findViewById(R.id.textView5);
+	        t.append(nationality);
+	        t = (TextView)findViewById(R.id.textView6);
+	        t.append(elo);
+        }
+        
 	}
 
-	@Override
-	public void handleServerError(String message) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void handleServerResponseData(JSONArray values) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void handleServerResponseMessage(String message) {
-		// TODO Auto-generated method stub
-	}
 }
