4b50b7dca56cf552104563c46f53de107ed9d2dd
Joel Ferm <jtferm@gmail.com>
2012-03-31 16:08:19 -0500

    packaged data from database in player list when player is clicked, to open up player status view

--- src/illinois/sweng/sctracker/PlayerListActivity.java
+++ src/illinois/sweng/sctracker/PlayerListActivity.java
@@ -1,6 +1,8 @@
 package illinois.sweng.sctracker;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
@@ -29,8 +31,15 @@ public class PlayerListActivity extends ListActivity {
 		startManagingCursor(mPlayerCursor);
 		
 		String fields[] = 	{
-								TrackerDatabaseAdapter.KEY_NAME, 
-								TrackerDatabaseAdapter.KEY_RACE
+				TrackerDatabaseAdapter.KEY_ROWID,
+				TrackerDatabaseAdapter.KEY_PK,
+				TrackerDatabaseAdapter.KEY_PICTURE,
+				TrackerDatabaseAdapter.KEY_HANDLE,
+				TrackerDatabaseAdapter.KEY_NAME,
+				TrackerDatabaseAdapter.KEY_RACE,
+				TrackerDatabaseAdapter.KEY_TEAM,
+				TrackerDatabaseAdapter.KEY_NATIONALITY,
+				TrackerDatabaseAdapter.KEY_ELO
 							};
 		int textViews[] = {R.id.playerListName, R.id.playerListRace};
 		
@@ -40,15 +49,67 @@ public class PlayerListActivity extends ListActivity {
 		setListAdapter(cursorAdapter);
 	}
 	
+	private void showPlayerStatus(Intent i) {
+		i.setClass(this, PlayerStatusActivity.class);
+		startActivity(i);
+	}
+	
 	private class PlayerListClickListener implements AdapterView.OnItemClickListener {
 
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			// TODO move cursor to this position and send data to player status activity
-			// pack data into an intent to send
 			mPlayerCursor.moveToPosition(position);
+			Intent i = new Intent();
+			Resources res = getResources();
 			
+			String rowKey = res.getString(R.string.keyRowID);
+			putIntExtra(rowKey, i);
 			
+			String pkKey = res.getString(R.string.keyPK);
+			putLongExtra(pkKey, i);
+			
+			String pictureKey = res.getString(R.string.keyPicture);
+			putStringExtra(pictureKey, i);
+			
+			String handleKey = res.getString(R.string.keyHandle);
+			putStringExtra(handleKey, i);
+			
+			String nameKey = res.getString(R.string.keyName);
+			putStringExtra(nameKey, i);
+			
+			String raceKey = res.getString(R.string.keyRace);
+			putStringExtra(raceKey, i);
+			
+			String teamKey = res.getString(R.string.keyTeam);
+			int teamIndex = mPlayerCursor.getColumnIndexOrThrow(teamKey);
+			int team = mPlayerCursor.getInt(teamIndex);
+			// TODO look up the team, not just send the team ID
+			i.putExtra(teamKey, team + "");
+			
+			String nationalityKey = res.getString(R.string.keyNationality);
+			putStringExtra(nationalityKey, i);
+			
+			String eloKey = res.getString(R.string.keyELO);
+			putIntExtra(eloKey, i);
+			
+			showPlayerStatus(i);
 		}
 		
+		private void putIntExtra(String key, Intent i) {
+			int index = mPlayerCursor.getColumnIndexOrThrow(key);
+			int rowID = mPlayerCursor.getInt(index);
+			i.putExtra(key, rowID);
+		}
+		
+		private void putStringExtra(String key, Intent i) {
+			int index = mPlayerCursor.getColumnIndexOrThrow(key);
+			String name = mPlayerCursor.getString(index);
+			i.putExtra(key, name);
+		}
+		
+		private void putLongExtra(String key, Intent i) {
+			int index = mPlayerCursor.getColumnIndexOrThrow(key);
+			long pk = mPlayerCursor.getLong(index);
+			i.putExtra(key, pk);
+		}
 	}
 }
