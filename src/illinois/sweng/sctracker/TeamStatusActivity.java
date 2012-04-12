a152c2d2612da509d9ebb74a20838c6995dee9b2
Chris Volk <chris.volk495@gmail.com>
2012-04-12 18:53:30 -0500

    for merge

--- src/illinois/sweng/sctracker/TeamStatusActivity.java
+++ src/illinois/sweng/sctracker/TeamStatusActivity.java
@@ -1,26 +1,33 @@
 package illinois.sweng.sctracker;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-public class TeamStatusActivity extends ListActivity {
+public class TeamStatusActivity extends ListActivity implements DelegateActivity{
 	static String TAG = "teamStatusActivity";
 
 	DBAdapter mDBAdapter;
 	Cursor mPlayerCursor;
-//>>>>>>> e3328afa3e04efdd01896a07062054d285ef7468
 	String name = "";
 	String teamTag = "";
+	long pk = -1;
 	int rowID = -1;
 	
 	@Override
@@ -32,12 +39,7 @@ public class TeamStatusActivity extends ListActivity {
 		
 		ListView listView = getListView();
 		listView.setTextFilterEnabled(true);
-//<<<<<<< HEAD
-//		listView.setOnItemClickListener(new TeamStatusClickListener());
-//=======
-		//TODO: Setup listener
 		listView.setOnItemClickListener(new PlayerListClickListener());
-//>>>>>>> e3328afa3e04efdd01896a07062054d285ef7468
 		
 		mDBAdapter = new DBAdapter(this);
 		mDBAdapter.open();
@@ -69,8 +71,40 @@ public class TeamStatusActivity extends ListActivity {
 					R.layout.playerlistrow, mPlayerCursor, fields, textViews);
 			
 			setListAdapter(cursorAdapter);
-					
+			
+			CheckBox favorite = (CheckBox) findViewById(R.id.checkBox1);
+			favorite.setChecked(isFavorite(pk));
+			favorite.setOnCheckedChangeListener(new FavoriteCheckboxClickHandler());
 
+	}
+	
+	private boolean isFavorite(long pk) {
+		String prefsName = getResources().getString(R.string.favoriteSharedPrefs);
+		SharedPreferences prefs = getSharedPreferences(prefsName, MODE_PRIVATE);
+		
+		// String favoriteKey = getResources().getString(R.string.favoritePlayerKey);
+		JSONArray def = new JSONArray();
+		
+		try {
+			String teamsKey = getResources().getString(R.string.favoriteTeamKey);
+			JSONArray favorites = new JSONArray(prefs.getString(teamsKey, def.toString()));
+			for(int i = 0; i < favorites.length(); i++) {
+				if(pk == favorites.getLong(i)) {
+					return true;
+				}
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+			Log.d(TAG, "Error reading favorite teams");
+		}
+		
+		return false;
+	}
+	
+	private void getFavoritesList() {
+		ServerCommunicator comm = new ServerCommunicator(this, TAG);
+		String userpass = "test@account.com:test";
+		comm.sendGetAllFavoritesRequest(userpass);
 	}
 	
 	private void getDataFromIntent(){
@@ -86,6 +120,9 @@ public class TeamStatusActivity extends ListActivity {
 		String rowIDKey = res.getString(R.string.keyRowID);
 		rowID = intent.getIntExtra(rowIDKey, -1);
 		
+		String pkKey = res.getString(R.string.keyPK);
+		pk = intent.getLongExtra(pkKey, -1);
+		
 	}
 	
 
@@ -93,59 +130,6 @@ public class TeamStatusActivity extends ListActivity {
 		i.setClass(this, PlayerStatusActivity.class);
 		startActivity(i);
 	}
-	
-/*<<<<<<< HEAD
-	private class TeamStatusClickListener implements AdapterView.OnItemClickListener {
-		
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			Log.d(TAG, "Entering player onclick");
-			players.moveToPosition(position);
-			Log.d(TAG, "after movetoposition");
-			Intent i = new Intent();
-			Log.d(TAG, "after declaring intent");
-			Resources res = getResources();
-			
-			Log.d(TAG, "before rowkey");
-			String rowKey = res.getString(R.string.keyRowID);
-			putIntExtra(rowKey, i);
-			
-//			String pkKey = res.getString(R.string.keyPK);
-//			putLongExtra(pkKey, i);
-			
-			Log.d(TAG, "before picKey");
-			String pictureKey = res.getString(R.string.keyPicture);
-			putStringExtra(pictureKey, i);
-			
-			Log.d(TAG, "before handlekey");
-			String handleKey = res.getString(R.string.keyHandle);
-			putStringExtra(handleKey, i);
-			
-			Log.d(TAG, "before namekey");
-			String nameKey = res.getString(R.string.keyName);
-			putStringExtra(nameKey, i);
-			
-			Log.d(TAG, "before racekey");
-			String raceKey = res.getString(R.string.keyRace);
-			putStringExtra(raceKey, i);
-			
-			Log.d(TAG, "before teamkey");
-			String teamKey = res.getString(R.string.keyTeam);
-			int teamIndex = players.getColumnIndexOrThrow(teamKey);
-			int team = players.getInt(teamIndex);
-			// TODO look up the team, not just send the team ID
-			i.putExtra(teamKey, team + "");
-			
-			Log.d(TAG, "before nationalitykey");
-			String nationalityKey = res.getString(R.string.keyNationality);
-			putStringExtra(nationalityKey, i);
-			
-			Log.d(TAG, "before elokey");
-			String eloKey = res.getString(R.string.keyELO);
-			putIntExtra(eloKey, i);
-			
-			Log.d(TAG, "Exiting onclick, going into player status");
-			showPlayerStatus(i);
-		}*/
 		
 
 	private class PlayerListClickListener implements AdapterView.OnItemClickListener {
@@ -220,4 +204,63 @@ public class TeamStatusActivity extends ListActivity {
 		mDBAdapter.close();
 	}
 
+	private class FavoriteCheckboxClickHandler implements CompoundButton.OnCheckedChangeListener {
+		public void onCheckedChanged(CompoundButton buttonView,	boolean isChecked) {
+			sendFavoriteRequest(isChecked);
+		}
+	}
+	
+	public void sendFavoriteRequest(boolean isChecked) {
+		ServerCommunicator com = new ServerCommunicator(this, TAG);
+		String userpass = "test@account.com:test";
+		if (isChecked) {
+			com.sendFavoritePlayerRequest(userpass, pk + "");
+		} else {
+			com.sendUnfavoritePlayerRequest(userpass, pk + "");
+		}
+		getFavoritesList();
+	}
+
+	public void handleServerError(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleServerResponseData(JSONArray values) {
+		Log.d(TAG, "Receiving favorites data");
+		
+		//String favoriteKey = getResources().getString(R.string.favoritePlayerKey);
+		
+		String prefsName = getResources().getString(R.string.favoriteSharedPrefs);
+		SharedPreferences prefs = getSharedPreferences(prefsName, MODE_PRIVATE);
+		SharedPreferences.Editor editor = prefs.edit();
+		
+		try {
+			JSONObject firstEntry = (JSONObject) (values.get(0));
+			
+			String eventKey = getResources().getString(R.string.favoriteEventKey);
+			JSONArray events = firstEntry.getJSONArray(eventKey);
+			editor.putString(eventKey, events.toString());
+			
+			String teamKey = getResources().getString(R.string.favoriteTeamKey);
+			JSONArray teams = firstEntry.getJSONArray(teamKey);
+			editor.putString(teamKey, teams.toString());
+			
+			String playerKey = getResources().getString(R.string.favoritePlayerKey);
+			JSONArray players = firstEntry.getJSONArray(playerKey);
+			editor.putString(playerKey, players.toString());
+			
+			editor.commit();
+		} catch (JSONException e) {
+			Log.d(TAG, "There was an error reading the JSON returned from the server");
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void handleServerResponseMessage(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
